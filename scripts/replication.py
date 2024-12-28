3a7866721dbb03fa124821a0722978d1b288cd55
Adam Amer <136176500+adamamer20@users.noreply.github.com>
2024-12-28 11:32:49 +0100

    abstracting classifer

--- scripts/replication.py
+++ scripts/replication.py
@@ -1,39 +1,60 @@
-from typing import Literal
+from typing import Literal, Optional
 
 import matplotlib.pyplot as plt
+import numpy as np
 import torch
+from scipy.integrate import solve_ivp
 from torch import nn
 from torch.optim import Adam
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
 
-TRAIN_SIZE = 60000
+torch.manual_seed(42)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
-# Define a traditional single-layer neural network
-class BPClassifier(nn.Module):
+class Classifier(nn.Module):
+    train_data_loader: DataLoader
+    test_data_loader: DataLoader
+    criterion: nn.CrossEntropyLoss
+    optimizer: Adam
+
+    input_size: int
+    hidden_size: int
+    output_size: int
+
     def __init__(
         self, dataset_name: Literal["MNIST"], minibatch_size: int, hidden_size: int
     ):
         super().__init__()
-
-        # Define general attributes
-        self.train_data_loader, self.test_data_loader = self.load_dataset(
+        self.train_data_loader, self.test_data_loader = self._load_dataset(
             dataset_name, minibatch_size
         )
-        self.criterion = nn.CrossEntropyLoss()
-        self.optimizer: Adam = None
 
-        # Define the layers
+        # Initialize hidden layer weights W (hidden_size x input_size)
         input_size = self.train_data_loader.dataset[0][0].shape[0]
-        output_size = self.train_data_loader.dataset[0][1].shape[0]
-        
-        self.fc1 = nn.Linear(input_size, hidden_size)
+        self.hidden_size = hidden_size
+        self.input_size = input_size
+        self.output_size = len(self.test_data_loader.dataset.classes)
+
+        # Initialize layers
         self.relu = nn.ReLU()
-        self.fc2 = nn.Linear(hidden_size, output_size)
         self.softmax = nn.Softmax(dim=1)
 
-    def load_dataset(
+        # Initialize training
+        self.train_errors = []
+        self.test_errors = []
+        self.criterion = nn.CrossEntropyLoss()
+
+        self.to(device)
+
+    def forward(x: torch.Tensor) -> torch.Tensor:
+        raise NotImplementedError
+
+    def train_and_plot_errors(self, learning_rate: float, epochs: int):
+        raise NotImplementedError
+
+    def _load_dataset(
         self, name: Literal["MNIST"], minibatch_size: int
     ) -> tuple[DataLoader, DataLoader]:
         if name == "MNIST":
@@ -45,25 +66,28 @@ class BPClassifier(nn.Module):
                 root="data", download=True, train=True, transform=transform
             )
 
-            self.train_dataloader = DataLoader(
+            train_dataloader = DataLoader(
                 train_dataset, batch_size=minibatch_size, shuffle=True
             )
-            self.test_dataloader = DataLoader(
+            test_dataloader = DataLoader(
                 datasets.MNIST(
                     root="data", download=True, train=False, transform=transform
                 ),
                 batch_size=minibatch_size,
                 shuffle=True,
             )
+            return train_dataloader, test_dataloader
+        else:
+            raise ValueError("Unsupported dataset")
 
-    def forward(self, x: torch.Tensor):
-        x = self.fc1(x)
-        x = self.relu(x)
-        x = self.fc2(x)
-        x = self.softmax(x)
-        return x
+    def _plot_errors(self):
+        plt.plot(self.train_errors, label=f"{self.__class__} Train Error")
+        plt.plot(self.test_errors, label=f"{self.__class__} Test Error")
+        plt.xlabel("Epochs")
+        plt.ylabel("Error Rate (%)")
+        plt.legend()
 
-    def run_epoch(
+    def _run_supervised_epoch(
         self,
         training: bool,
     ) -> float:
@@ -75,6 +99,7 @@ class BPClassifier(nn.Module):
         total = 0
         data_loader = self.train_data_loader if training else self.test_data_loader
         for data, target in data_loader:
+            data, target = data.to(device), target.to(device)
             if training and self.optimizer is not None:
                 self.optimizer.zero_grad()
             outputs = self(data)
@@ -89,34 +114,53 @@ class BPClassifier(nn.Module):
         print(f"{'Train' if training else 'Test'} Error: {error_rate}%")
         return error_rate
 
-    def train_and_plot_errors(
+    def _train_supervised(
         self,
         learning_rate: float,
         epochs: int,
     ):
+        print("Starting Supervised Learning Phase")
         self.optimizer = Adam(self.parameters(), lr=learning_rate)
-        train_errors = []
-        test_errors = []
         for epoch in range(epochs):
             print(f"Epoch {epoch+1}/{epochs}")
-            train_error = self.run_epoch(training=True)
-            train_errors.append(train_error)
-            test_error = self.run_epoch(training=False)
-            test_errors.append(test_error)
-        self.plot_errors(train_errors, test_errors)
+            train_error = self._run_supervised_epoch(training=True)
+            self.train_errors.append(train_error)
+            test_error = self._run_supervised_epoch(training=False)
+            self.test_errors.append(test_error)
+        print("Supervised Learning Phase Complete")
 
-    def plot_errors(self, train_errors: list[float], test_errors: list[float]):
-        plt.figure(figsize=(10, 5))
-        plt.plot(train_errors, label="Train Error")
-        plt.plot(test_errors, label="Test Error")
-        plt.xlabel("Epochs")
-        plt.ylabel("Error Rate (%)")
-        plt.legend()
-        plt.title("Training and Test Error Rates")
-        plt.show()
+
+# Define a traditional single-layer neural network
+class BPClassifier(Classifier):
+    def __init__(
+        self, dataset_name: Literal["MNIST"], minibatch_size: int, hidden_size: int
+    ):
+        super().__init__(
+            dataset_name=dataset_name,
+            minibatch_size=minibatch_size,
+            hidden_size=hidden_size,
+        )
+
+        # Define the neural network architecture
+        self.fc1 = nn.Linear(self.input_size, self.hidden_size)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(self.hidden_size, self.output_size)
+        self.softmax = nn.Softmax(dim=1)
+
+    def forward(self, x: torch.Tensor):
+        x = self.fc1(x)
+        x = self.relu(x)
+        x = self.fc2(x)
+        x = self.softmax(x)
+        return x
+
+    def train_and_plot_errors(self, learning_rate: float, epochs: int):
+        self._train_supervised(learning_rate=learning_rate, epochs=epochs)
+        self._plot_errors()
+
 
 
 if __name__ == "__main__":
-    model = BPClassifier(input_size=784, hidden_size=2000, output_size=10)
-    model.load_dataset("MNIST", 64)
-    model.train_and_plot_errors(learning_rate=0.001, epochs=300)
+    model = BPClassifier("MNIST", minibatch_size=64, hidden_size=2000).to(device)
+    model.train_and_plot_errors(learning_rate=0.001, epochs=100)
+
